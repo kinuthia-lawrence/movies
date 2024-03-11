b38f843f066a4561bd8bc42986564af2dd84c862
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-11 08:43:44 +0300

    Initial commit

---
+++ movies/src/main/java/com/larrykin343/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package com.larrykin343.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
---
+++ movies/src/test/java/com/larrykin343/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larrykin343.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
